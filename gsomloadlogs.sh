5515b9d056dd246b63e16f22f746ee689df5792a
Vasiliy V. Garshin <50753438+vgarshin@users.noreply.github.com>
2020-10-04 00:00:28 +0300

    Update gsomloadlogs.sh

--- gsomloadlogs.sh
+++ gsomloadlogs.sh
@@ -5,7 +5,7 @@ cd `dirname "$0"`
 # Yandex.Metrica logs load to ClickHouse script
 
 # hits logs
-python3 /home/logsloader/gsomlogs/logs_api_integration/metrica_logs_api.py -mode regular -source hits
+python3 /home/logsloader/gsomlogs/logs_api_integration/metrica_logs_api.py -mode regular -source hits >> gsomlogs.log
 
 # visits log
-python3 /home/logsloader/gsomlogs/logs_api_integration/metrica_logs_api.py -mode regular -source visits
+python3 /home/logsloader/gsomlogs/logs_api_integration/metrica_logs_api.py -mode regular -source visits >> gsomlogs.log
